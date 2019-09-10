68f4651663bf937ad49b8c8af8c0ed8de101b301
2507912924 <2507912924@qq.com>
2019-09-10 21:22:54 +0800

    1231

--- offercode/main.cpp
+++ offercode/main.cpp
@@ -773,7 +773,20 @@ int main() {
 	return 0;
 }
 
-
+int maxSubArray(vector<int>& nums) {
+	int ans = nums[0];
+	int sum = 0;
+	for (int num : nums) {
+		if (sum > 0) {
+			sum += num;
+		}
+		else {
+			sum = num;
+		}
+		ans = max(ans, sum);
+	}
+	return ans;
+}
 
 
 
