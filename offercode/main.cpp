02d095d18e846e88c2d1d0f270612ef6a2fc0fa1
2507912924 <2507912924@qq.com>
2019-09-11 20:17:32 +0800

    33

--- offercode/main.cpp
+++ offercode/main.cpp
@@ -736,6 +736,33 @@ int main() {
 
 }
 
+public class LeastMovePass {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		int[] sortArr = new int[n];
+		for (int i = 0; i < n; i++) {
+
+			arr[i] = scanner.nextInt();
+			sortArr[i] = arr[i];
+		}
+		Arrays.sort(sortArr);
+		int count = 0;
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+
+			if (arr[i] == sortArr[j]) {
+				++count;
+				++j;
+			}
+		}
+		System.out.println(n - count);
+	}
+}
 
 
 
