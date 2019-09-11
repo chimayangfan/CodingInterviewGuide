48fcd393dc97ae8ee4bc326730ad2820202912b5
2507912924 <2507912924@qq.com>
2019-09-11 20:27:20 +0800

    55

--- offercode/main.cpp
+++ offercode/main.cpp
@@ -803,6 +803,71 @@ int main()
 	return 0;
 }
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+template <typename T>
+int GetMinimumSwapsForSorted(T seq[], int n)
+{
+	bool* right_place_flag = new bool[n];
+	T* sorted_seq = new T[n];
+	int p, q;
+	////
+	copy(seq, seq + n, sorted_seq);
+	sort(sorted_seq, sorted_seq + n);    ////可采用效率更高的排序算法
+										 // 初始化bitarray
+	for (int i = 0; i < n; i++)
+	{
+		if (seq[i] != sorted_seq[i])
+			right_place_flag[i] = false;
+		else
+			right_place_flag[i] = true;
+	}
+	////
+	p = 0;
+	int minimumswap = 0;
+	while (1)
+	{
+		while (right_place_flag[p])
+			p++;
+		q = p + 1;
+		// 在已排好序的数组中找到和未排好序的元素，此种找法只对无重复序列能得出minimum swaps
+		while (q < n)
+		{
+			if (!right_place_flag[q] && sorted_seq[q] == seq[p])
+				break;
+			q++;
+		}
+		if (q >= n || p >= n)
+			break;
+		right_place_flag[q] = true;
+		// 对调后正好在排好序数组的位置上，则bitarray中的那位也置为true
+		if (seq[q] == sorted_seq[p])
+			right_place_flag[p] = true;
+		swap(seq[p], seq[q]);
+		minimumswap++;
+	}
+
+	delete[] sorted_seq;
+	delete[] right_place_flag;
+
+	return minimumswap;
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int seq[] = { 3, 2, 1, 5, 6, 8, 4, 7 };//{2,3,1,5,6,4};//{2,3,2,4,7,6,3,5};
+	int n = sizeof(seq) / sizeof(int);
+	cout << "minimum swaps : " << GetMinimumSwapsForSorted(seq, n) << endl;
+
+	system("pause");
+	return 0;
+}
+
 
 
 
