9a77476e24cd029562c9927ce28fa8ef35a69b78
2507912924 <2507912924@qq.com>
2019-09-11 17:09:51 +0800

    421

--- offercode/main.cpp
+++ offercode/main.cpp
@@ -726,26 +726,27 @@
 
 using namespace std;
 
-string minWindow(string s, string t) {
-	int count[256] = { 0 };
-	for (auto c : t) ++count[c];
-	int len = 0, minLength = s.length();
-	string res;
-	for (int l = 0, r = 0; r < s.length(); ++r) {
-		count[s[r]]--;
-		if (count[s[r]] >= 0) ++len;
-		while (len == t.length()) {
-			if (r - l + 1 <= minLength) {
-				minLength = r - l + 1;
-				res = s.substr(l, r - l + 1);
-			}
-			count[s[l]]++;
-			if (count[s[l]] > 0) --len;
-			++l;
+public static  int test(String s) {
+	int res = 0;
+	int n = s.length();
+	LinkedList<Character> stack = new LinkedList<>();
+	for (int i = 0; i < n; i++) {
+		char c = s.charAt(i);
+		if (c == '(') {
+			stack.push(c);
+			res++;
+		}
+		if (c == ')') {
+			stack.pop();
+			res--;
+		}
+		if (c == '0') {
+			break;
 		}
 	}
 	return res;
 }
+
 
 int main() {
 	string str1, str2;
