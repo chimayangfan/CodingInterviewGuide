a3c130e92f94bb3e1c8dde3631224a3973670e4d
2507912924 <2507912924@qq.com>
2019-09-16 20:30:16 +0800

    141

--- offercode/xxc.cpp
+++ offercode/xxc.cpp
@@ -96,3 +96,18 @@ string find_longest_num_str(string input)
 	return  output;
 
 }
+
+
+class Solution {
+	public int smallestRangeII(int[] A, int K) {
+		Arrays.sort(A);
+		int n = A.length;
+		int res = A[n - 1] - A[0];
+		for (int i = 1; i<n; i++) {
+			int min = Math.min(A[0] + K, A[i] - K);
+			int max = Math.max(A[n - 1] - K, A[i - 1] + K);
+			res = Math.min(max - min, res);
+		}
+		return res;
+	}
+}
