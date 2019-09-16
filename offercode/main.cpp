a0e0ab27411b609481717d6aede9990e2c1504a8
2507912924 <2507912924@qq.com>
2019-09-16 20:11:48 +0800

    9.16

--- offercode/main.cpp
+++ offercode/main.cpp
@@ -807,42 +807,28 @@
 
 using namespace std;
 
-int minPathSum(vector<vector<int> > &grid) {
-	int m = grid.size();
-	if (m == 0)
-		return 0;
-	int n = grid[0].size();
-	vector<vector<int> > dp(m, vector<int>(n));
-	dp[0][0] = grid[0][0];
-	for (int i = 1; i < m; i++)
-		dp[i][0] = dp[i - 1][0] + grid[i][0];
-	for (int i = 1; i < n; i++)
-		dp[0][i] = dp[0][i - 1] + grid[0][i];
-	for (int i = 1; i < m; i++)
-	{
-		for (int j = 1; j < n; j++)
-		{
-			dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-		}
-	}
-	return dp[m - 1][n - 1];
-
-}
-
 int main()
 {
-	int m,n;
-	cin >> m >> n;
-	vector<vector<int> > grid(m,vector<int>(n,0));
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cin >> grid[i][j];
-		}
+	int n, s;
+	int MAX = 1e10;
+	scanf("%d%d", &n, &s);
+	vector<int> Input;
+	int Result = MAX;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		Input.push_back(tmp);
 	}
-	cout << minPathSum(grid) << endl;
-
+	sort(Input.begin(), Input.end());
+	for (int i = 0; i < Input.size() - 1; i++) {
+		int Left = min(Input[0] + s, Input[i + 1] - s);
+		int Right = max(Input[i] + s, Input[Input.size() - 1] - s);
+		Result = min(Result, Right - Left);
+	}
+	printf("%d\n", Result);
 	return 0;
 }
+
 
 
 
